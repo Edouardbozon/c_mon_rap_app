f27027663b9cc7f6fe731d1995a644ca3b77ea27
edouardbozon <bozon.edouard@live.fr>
2016-03-17 22:49:16 +0100

    Add navbar and routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,6 +6,21 @@ export function routerConfig ($routeProvider) {
       controller: 'MainController',
       controllerAs: 'main'
     })
+    .when('/users', {
+      templateUrl: 'app/main/main.html',
+      controller: 'UsersController',
+      controllerAs: 'users'
+    })
+    .when('/matches', {
+      templateUrl: 'app/main/main.html',
+      controller: 'MatchesController',
+      controllerAs: 'matches'
+    })
+    .when('/messages', {
+      templateUrl: 'app/main/main.html',
+      controller: 'MessageController',
+      controllerAs: 'message'
+    })
     .otherwise({
       redirectTo: '/'
     });
