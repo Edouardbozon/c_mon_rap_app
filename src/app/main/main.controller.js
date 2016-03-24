afcf5920fed75f0894ee9411bd8074099974bfae
edouardbozon <bozon.edouard@live.fr>
2016-03-24 23:37:29 +0100

    Catch errors from async XHR call

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -7,23 +7,34 @@ export class MainController {
         this.MainService = MainService;
         this.$log = $log;
 
-        $rootScope.user = {};
-        $rootScope.user.isConnected = false;
-        $rootScope.user.fbToken = undefined;
+        $rootScope.user = {
+            id: undefined,
+            name: undefined,
+            isConnected: false,
+            fbToken: undefined
+        };
+    }
 
+    logTinder(){
+        this.MainService.authTinder().then((response) => {
+            this.$log(response);
+        })
     }
 
     logFacebook(){
         if(this.$rootScope.user.isConnected === false){
             this.MainService
             .logFacebook()
-            .then((response)=>{
+            .then((response) => {
                 if(response.status === 'connected'){
                     this.getUserId();
 
                     this.$rootScope.user.fbToken = response.authResponse.accessToken;
                     this.$rootScope.user.isConnected = true;
                 }
+            })
+            .catch((error) => {
+                this.$log.error('XHR Failed to log in Facebook API.\n' + angular.toJson(error.data, true));
             });
         }
     }
@@ -31,24 +42,23 @@ export class MainController {
     getUserId(){
         this.MainService
         .getUserId()
-        .then((response)=>{
+        .then((response) => {
             this.$rootScope.user.id = response.id;
             this.$rootScope.user.name = response.name;
             this.getProfilePicture();
+        }).catch((error) => {
+            this.$log.error('XHR Failed to get userID from Facebook API.\n' + angular.toJson(error.data, true));
         });
     }
 
     getProfilePicture(){
         this.MainService
         .getProfilePicture()
-        .then((response)=>{
+        .then((response) => {
             this.$rootScope.user.picture = response.data.url;
+        }).catch((error) => {
+            this.$log.error('XHR Failed to get profile picture from Facebook API.\n' + angular.toJson(error.data, true));
         });
     }
 
-    logTinder(){
-        this.MainService.authTinder().then((response)=>{
-            this.$log(response);
-        })
-    }
 }
